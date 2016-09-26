325e16d7b22611d49e05a39987f7f67d132cf62d
Cristobal Romero <cristobalromerorossi@gmail.com>
2016-09-26 16:05:45 -0500

    ADD mediaplayer

---
+++ app/src/main/java/com/crom/miguiapp/services/MediaPlayerService.java
@@ -0,0 +1,98 @@
+package com.crom.miguiapp.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.crom.miguiapp.AudioActivity;
+import com.crom.miguiapp.R;
+import com.crom.miguiapp.task.PlayBackTask;
+
+import java.io.IOException;
+
+/**
+ * Created by Cristobal Romero on 25/09/2016.
+ */
+public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener {
+    public static final String ACTION_PLAY = "com.crom.miguiapp.action.PLAY";
+    private final String TAG = this.getClass().getSimpleName();
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.maluma_el_perdedor);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try{
+            if (intent.getAction().equals(ACTION_PLAY)) {
+                mediaPlayer.setOnPreparedListener(this);
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            }
+        }catch (Exception e){
+            Log.e(getClass().getSimpleName(), e.toString());
+            //this.onDestroy();
+            stopSelf();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer player) {
+        player.start();
+        getNotification();
+    }
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "stop Service");
+        mediaPlayer.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+private void getNotification(){
+    NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_play_dark)
+                    .setContentTitle(getString(R.string.notify_title))
+                    .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+    Intent resultIntent = new Intent(this, AudioActivity.class);
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+    stackBuilder.addParentStack(AudioActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+    stackBuilder.addNextIntent(resultIntent);
+    PendingIntent resultPendingIntent =
+            stackBuilder.getPendingIntent(
+                    0,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+    mBuilder.setContentIntent(resultPendingIntent);
+    NotificationManager mNotificationManager =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+    mNotificationManager.notify(0, mBuilder.build());
+}
+}
